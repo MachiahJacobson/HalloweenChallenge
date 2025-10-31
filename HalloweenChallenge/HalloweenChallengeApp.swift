cea98c69d5826800770ee04c4d115c213dbafbd3
Mac <macjacobson99@gmail.com>
2025-10-31 14:11:13 -0600

    Initial Commit

---
+++ HalloweenChallenge/HalloweenChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalloweenChallengeApp.swift
+//  HalloweenChallenge
+//
+//  Created by Jacobson, Machiah - Student on 10/31/25.
+//
+
+import SwiftUI
+
+@main
+struct HalloweenChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HalloweenChallengeTests/HalloweenChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  HalloweenChallengeTests.swift
+//  HalloweenChallengeTests
+//
+//  Created by Jacobson, Machiah - Student on 10/31/25.
+//
+
+import Testing
+@testable import HalloweenChallenge
+
+struct HalloweenChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
